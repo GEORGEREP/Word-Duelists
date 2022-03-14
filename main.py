408ec15ee4e976bf3f8420988785cb87a1c8a61e
GEORGEREP <georgerep110@gmail.com>
2022-03-14 11:52:20 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame as pg
+import random
+
+FPS = 60
+
+WIDTH, HEIGHT = 1280, 720
+WINDOW = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Word Duelists")
+
+pg.font.init()
