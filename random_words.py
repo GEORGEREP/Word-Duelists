5ca01d1d40625c0bf95cb1dcea390085d05d09b4
GEORGEREP <95755065+GEORGEREP@users.noreply.github.com>
2022-05-13 18:13:09 +0300

    Delete random_words.py

--- random_words.py
+++
@@ -1,7 +0,0 @@
-import random
-from english_words import english_words_lower_alpha_set
-words=[]
-for i in english_words_lower_alpha_set:
-    if len(i)==5:
-        words.append(i)
-print(random.choice(words))
